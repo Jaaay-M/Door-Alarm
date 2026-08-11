ccefba346c4c71864008c2a7abce18f9b485f432
Jay <jaymanning123@gmail.com>
2026-08-11 20:38:41 +0100

    I have setup the ultrasonic sensor and buzzer in the code. The ultrasonic sensor is connected to pins 2 and 3, while the buzzer is connected to pin 8. The code measures the distance using the ultrasonic sensor and if the distance is less than 10 cm, it will activate the buzzer with alternating tones of 1000 Hz and 750 Hz every 500 milliseconds.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Gives the pin numbers for the ultrasonic sensor
+#define echoPin 2
+#define trigPin 3
+
+// Variables for the ultrasonic sensor
+long duration;
+float distance;
+
+const int buzzer = 8; // Buzzer connected to pin 9
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  // Opens port 9600 for serial communication
+  Serial.begin(9600);
+
+  // Sets the trigPin as an OUTPUT and the echoPin as an INPUT
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // Sets the buzzer pin as an OUTPUT
+  pinMode(buzzer, OUTPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Waits for 1 second before the next loop
+
+  // If the distance is less than 10 cm, the buzzer will sound
+  while (distance < 10) {
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(500); // Wait for 500 milliseconds
+    tone(buzzer, 750); // Send 1KHz sound signal...
+    delay(500); // Wait for 500 milliseconds
+  }
+
 }
